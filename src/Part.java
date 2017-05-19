a008fc77126028b2cacf8a8916e0330104264fab
Kseniya Torgaeva <torgaeva.ksenia@yandex.ru>
2017-05-19 22:27:59 +0300

    Gigignore was added

--- src/Part.java
+++
@@ -1,6 +0,0 @@
-//@Marker
-public abstract class Part {// TODO: добавить поле String - формула и парсить её...
-    abstract int getRightBoard();
-    abstract int getLeftBoard();
-    abstract double calculateY(double x);
-}
