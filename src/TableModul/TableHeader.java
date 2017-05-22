f42ab12b1a5239a891dafb972745a2cb76f02199
Andrey Sarichev <sarichevav@mail.ru>
2017-05-22 22:33:09 +0300

    Add TableModul

---
+++ src/TableModul/TableHeader.java
@@ -0,0 +1,11 @@
+public class TableHeader {
+    private String headerTitel;
+
+    public TableHeader(String headerTitel) {
+        this.headerTitel = headerTitel;
+    }
+
+    public String getHeaderTitel() {
+        return headerTitel;
+    }
+}
